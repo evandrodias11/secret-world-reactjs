0202573e115a050f07b214d106562416d9cce72a
evandrodias11 <evandro.dias@paripassu.com.br>
2024-07-01 15:37:13 -0300

    tira paragro e centraliza tudo na tela

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -4,7 +4,6 @@ const StartScreen = ({ startGame }) => {
   return (
     <div className="start">
       <h1>Secret Word</h1>
-      <p>Clique no botão abaixo para jogar</p>
       <button onClick={startGame}>Começar o jogo</button>
     </div>
   );
