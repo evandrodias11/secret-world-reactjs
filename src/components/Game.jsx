1caddb34b2cd839efbcdde206a7fd8e9d0af1b85
evandrodias11 <evandro.dias@paripassu.com.br>
2024-07-01 15:05:49 -0300

    adiciona observação e letras erradas sempre maiusuculas

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -44,6 +44,7 @@ const Game = ({
       </div>
       <div className="letterContainer">
         <p>Tente adivinhar uma letra da palavra:</p>
+        <p className="observation">obs.: digitar acento</p>
         <form onSubmit={handleSubmit}>
           <input
             type="text"
@@ -60,7 +61,7 @@ const Game = ({
       <div className="wrongLettersContainer">
         <p>Letras erradas:</p>
         {wrongLetters.map((letter, i) => (
-          <span key={i}>{letter}, </span>
+          <span key={i}>{letter.toUpperCase()}, </span>
         ))}
       </div>
     </div>
